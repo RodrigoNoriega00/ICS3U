89de6f305fb21d3f93904de4a9ead1275184451b
Rodrigo Noriega <323995084@C-3ZYDYR1.tdsb.on.ca>
2017-02-21 14:45:32 -0500

    Pushed "Hello,World!" program

---
+++ Hello/src/noriegaMestanza/HelloWorld.java
@@ -0,0 +1,15 @@
+package noriegaMestanza;
+/**
+ * Helloworld.Java
+ * Outputs "Hello,World!" to console
+ * February, 21,2017
+ * @author Rodrigo Noriega
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,World!");
+
+	}
+
+}
