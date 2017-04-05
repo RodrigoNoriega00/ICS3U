54c96564ec1177700e3d5e758b5fc077e79bad2d
Rodrigo Noriega <323995084@C-3ZYDYR1.tdsb.on.ca>
2017-04-05 15:09:40 -0400

    Finished programming assignment

--- Unit2/src/noriegaMestanza/largestNumber.java
+++ Unit2/src/noriegaMestanza/largestNumber.java
@@ -20,14 +20,26 @@ public class largestNumber {
 		// TODO Auto-generated method stub
 
 		Scanner scan=new Scanner (System.in);
-		System.out.print("Enter 2 numbers");
-		int b=scan.nextInt();
-		int a=scan.nextInt();
-		int large= largeN (a,b);
-		System.out.print(large);
+		int[] array=new int [4];
+		System.out.print("Enter 4 numbers");
+		array[0]=scan.nextInt();
+		array[1]=scan.nextInt();
+		array[2]=scan.nextInt();
+		array[3]=scan.nextInt();
+		int num=scan.nextInt();
+		int bigN= largeN(array);
+		System.out.println(bigN);
+		
+		}
+	public static int largeN (int[] array){
+		int largestN=array[0];
+		for (int i=1;i<array.length;i++){
+			if(array[i]>array[largestN]){
+				largestN=i;
+			}
+		}
+		return largestN;
 	}
-	public static int largeN (int a,int b){
-		return Math.max(a,b);
-	}
+		
 
 }
